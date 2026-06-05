9bbdbe0e8776f416dfc47aed383cda05282b47f6
Tavishbedii <tavishbedi4@gmail.com>
2026-06-06 02:02:27 +0530

    Add files via upload

---
+++ ReshapeTheMatrix.c
@@ -0,0 +1,29 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** matrixReshape(int** mat, int matSize, int* matColSize, int r, int c, int* returnSize, int** returnColumnSizes) {
+    int m = matSize,n=matColSize[0];
+    if(m*n != r*c){
+        *returnSize = m;
+        *returnColumnSizes = malloc(m* sizeof(int));
+        for(int i = 0;i<m;i++)
+        (*returnColumnSizes)[i] = n;
+        return mat;
+    }
+    int **res = malloc(r* sizeof(int*));
+    *returnColumnSizes = malloc(r* sizeof(int));
+    
+    for(int i =0;i<r;i++){
+        res[i]=malloc(c* sizeof(int));
+        (*returnColumnSizes)[i]=c;
+    }
+    for(int i =0;i<m*n;i++)
+    res[i/c][i%c] = mat[i/n][i%n];
+
+    *returnSize = r;
+    return res;
+
+    
+}
